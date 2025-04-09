b19bf940a8df1afaa2072c2f60115ade46f0fa9a
M4theusS0uz4 <matheusrocham.ms@gmail.com>
2025-04-09 14:36:45 -0300

    Fixing user routes

--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { register, login } from '../services/user-service/userController.js';
-
-const router = Router();
-
-router.post('/register', register); 
-router.post('/login', login)
-
-export default router;
